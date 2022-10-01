b2d8c3fb24b69d010a1a7785f3e033945ecb411b
Yusuke Kawamoto <novogrammer@gmail.com>
2022-10-02 04:42:41 +0900

    clickが面倒なので開発中は自動clickする。

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,9 +3,13 @@ import App from "./App";
 
 
 window.addEventListener("load",()=>{
-  const startElement=document.querySelector(".c-start");
+  const startElement=document.querySelector(".c-start") as HTMLDivElement;
   startElement.addEventListener("click",()=>{
     window.app=new App();
     startElement.classList.add("c-start--hidden");  
   });
+  if (process.env.NODE_ENV !== 'production') {
+    startElement.click();
+
+  }
 });
